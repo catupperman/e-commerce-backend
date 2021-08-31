7388b5ca21bdf97fe0c5238cdda6d9ff8c1563f1
catupperman <catupperman@gmail.com>
2021-08-30 21:27:38 -0400

    missed the index.js model

--- models/index.js
+++ models/index.js
@@ -6,18 +6,25 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  as: 'product_details'
+  foreignKey: 'category_id',
 });
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+})
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+}
+)
 
 module.exports = {
   Product,
